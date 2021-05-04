af57b971cb15112e313c0a2d156280437a282443
shuchitasingh <singhshuchita5@gmail.com>
2021-05-04 16:29:34 +0530

    Added:Binary search method

---
+++ src/main/java/com/bridgelabz/BinaryTree.java
@@ -0,0 +1,53 @@
+package com.bridgelabz;
+
+
+public class BinaryTree<K extends Comparable<K>> {
+    private BinaryNode<K> root;
+
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    public BinaryNode<K> addRecursively(BinaryNode<K> current, K key) {
+        if (current == null)
+            return new BinaryNode<>(key);
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) {
+            return current;
+        }
+        if (compareResult < 0) {
+            current.left = addRecursively(current.left, key);
+        } else {
+            current.right = addRecursively(current.right, key);
+        }
+        return current;
+    }
+
+    public int getSize() {
+        return this.getSizeRecursive(root);
+    }
+
+    public int getSizeRecursive(BinaryNode<K> current) {
+        return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
+                + this.getSizeRecursive(current.right);
+    }
+
+    public boolean searchNode(K key){
+        return search(root,key);
+    }
+
+    public  boolean search(BinaryNode current,K nodeToBeSearched){
+        if(current==null){
+            return false;
+        }if(current.key== nodeToBeSearched){
+            return true;
+        }
+        boolean result=false;
+        if(current.key.compareTo(nodeToBeSearched)>0){
+            result=search(current.left,nodeToBeSearched);
+        }else if(current.key.compareTo(nodeToBeSearched)<0){
+            result=search(current.right,nodeToBeSearched);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/bridgelabz/BinaryNodeTest.java
@@ -0,0 +1,57 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BinaryNodeTest {
+    private Assertions Assert;
+
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int size = binaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
+
+    @Test
+    public void given13_NumbersWhenaddedToTheBinaryTree_ShouldReturnSize13() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(65);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        int size = binaryTree.getSize();
+        Assert.assertEquals(13, size);
+    }
+    @Test
+    public void given13_NumbersWhenSearchInTheBinaryTree_ShouldReturnTrue(){
+        BinaryTree<Integer> binaryTree=new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        binaryTree.add(22);
+        binaryTree.add(40);
+        binaryTree.add(60);
+        binaryTree.add(95);
+        binaryTree.add(11);
+        binaryTree.add(3);
+        binaryTree.add(16);
+        binaryTree.add(65);
+        binaryTree.add(63);
+        binaryTree.add(67);
+        boolean result= binaryTree.searchNode(63);
+        Assert.assertTrue(result);
+    }
+}
